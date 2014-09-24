64255211509b2e019352730908a35923397b9c0e
Brendon Boshell <brendonboshell@gmail.com>
2014-09-24 16:42:13 +0100

    new version

--- README.md
+++ README.md
@@ -1,49 +1,58 @@
 # RespBreak
 
-Handles breakpoints in responsive designs, using a pattern inspired by Ruby on Rails up()-down() DB Migrator pattern.
+RespBreak allows you to easily handle breakpoints in Javascript, to match up
+with your CSS media queries.
 
-## Usage
-
-For each state you want to represent (e.g. mobile, tablet, desktop) create an object with three methods:
-
-* `ishere(width)` evaluates criteria and returns `true` if the browser window is in a particular state. This method is passed the window width as the `width` parameter. A typical condition is `width <= 976` for example.
-* `up()` is called when the browser window moves "up" a state (e.g. from mobile to tablet). This function should do all the necessary work to change the document flow and make the changes you want.
-* `down()` is called when the browser window moves "down" a state (e.g. from tablet to mobile). It should undo all the changes that `up()` did.
-
-When you have all the state objects you desire, simply create a new `RespBreak` object: `new RespBreak(states, loaded_version)`. The `states` parameter is an array of your state objects. `loaded_version` is the array index of the state object you have currently loaded (e.g. if you pass `[state_mobile, state_desktop]` as `states`, and your HTML is served for mobile first, you pass `loaded_version=0`. If your HTML is served for desktop first, you pass `loaded_version=1`).
-
-### Example
-
-This example is for when HTML is served mobile first. The HTML document is structured for the mobile view. When the browser window size is increased beyond 976px wide, the `state_full.up()` function is called, and the function makes the necessary structural changes for the desktop website. When the window decreases, `state_full.down()` is called to undo the changes.
-
-If the desktop HTML is served by default, you can simply change the second parameter of the RespBreak constructor to `1`, corresponding to that state object's position in the array `states`.
+## Responsive Images Example
 
 HTML:
 
-    <div id="output">mobile</div>
+    <p>
+      <img src="test-450-mobile.jpg" style="width: 100%; max-width: 1024px;"
+        data-version-tablet-src="test-700.jpg"
+        data-version-desktop-src="test-1024.jpg"
+      >
+    </p>
 
 Javascript:
 
-    var state_mobile = {
-        "ishere": function (width) {
-            return (width <= 976);
+    var replaceImgs = function (form) {
+      $("img").each(function () {
+        var $this = $(this),
+            versionSrc = $this.data("version-" + form + "-src"),
+            versionWidth = $this.data("version-" + form + "-width"),
+            versionHeight = $this.data("version-" + form + "-height");
+
+        if (!$this.data("version-mobile-src")) {
+          // copy the mobile version state data
+          $this.attr("data-version-mobile-src", $this.attr("src"));
+          $this.attr("data-version-mobile-width", $this.attr("width"));
+          $this.attr("data-version-mobile-height", $this.attr("height"));
         }
-    };
-    
-    var state_full = {
-        "ishere": function (width) {
-            return (width > 976);
-        },
-        
-        // up from mobile
-        "up": function () {
-            document.getElementById("output").innerHTML = "desktop";
-        },
-        
-        // down to mobile
-        "down": function () {
-             document.getElementById("output").innerHTML = "mobile";
+
+        if (versionSrc) {
+          $this.attr("src", versionSrc);
         }
+
+        if (versionWidth) {
+          $this.width(versionWidth);
+        }
+
+        if (versionHeight) {
+          $this.height(versionHeight);
+        }
+      });
     };
-    
-    new RespBreak([state_mobile, state_full], 0);
+
+    RespBreak.addState("mobile", 0, 450);
+    RespBreak.addState("tablet", 451, 700);
+    RespBreak.addState("desktop", 701);
+    RespBreak.enter("mobile", function () {
+      replaceImgs("mobile");
+    });
+    RespBreak.enter("tablet", function () {
+      replaceImgs("tablet");
+    });
+    RespBreak.enter("desktop", function () {
+      replaceImgs("desktop");
+    });
--- RespBreak.js
+++ RespBreak.js
@@ -1,170 +1,177 @@
-/*! viewportSize | Author: Tyson Matanich, 2013 | License: MIT */
-(function (window) {
+var RespBreak = {
+	/*
+		The _getWidth function is based on code from 'viewportSize'
+		by Tyson Matanich, released under the MIT license.
 
-	window.viewportSize = {};
-
-	window.viewportSize.getHeight = function () {
-		return getSize("Height");
-	};
-
-	window.viewportSize.getWidth = function () {
-		return getSize("Width");
-	};
-	
-	var is_client_width = null;
-
-	var getSize = function (Name) {
-		var size;
-		var name = Name.toLowerCase();
-		var document = window.document;
-		var documentElement = document.documentElement;
-		if (window["inner" + Name] === undefined) {
-			// IE6 & IE7 don't have window.innerWidth or innerHeight
-			size = documentElement["client" + Name];
-		}
-		else if (window["inner" + Name] != documentElement["client" + Name]) {
-			// WebKit doesn't include scrollbars while calculating viewport size so we have to get fancy
-			
-			if (is_client_width !== null) {
-				if (is_client_width) {
-					size = documentElement["client" + Name];
-				} else {
-					size = window["inner" + Name];
-				}
-			} else {
-				// Insert markup to test if a media query will match document.doumentElement["client" + Name]
-				var bodyElement = document.createElement("body");
-				bodyElement.id = "vpw-test-b";
-				bodyElement.style.cssText = "overflow:scroll";
-				var divElement = document.createElement("div");
-				divElement.id = "vpw-test-d";
-				divElement.style.cssText = "position:absolute;top:-1000px";
-				// Getting specific on the CSS selector so it won't get overridden easily
-				divElement.innerHTML = "<style>@media(" + name + ":" + documentElement["client" + Name] + "px){body#vpw-test-b div#vpw-test-d{" + name + ":7px!important}}</style>";
-				bodyElement.appendChild(divElement);
-				documentElement.insertBefore(bodyElement, document.head);
-
-				if (divElement["offset" + Name] == 7) {
-					// Media query matches document.documentElement["client" + Name]
-					size = documentElement["client" + Name];
-					is_client_width = true;
-				}
-				else {
-					// Media query didn't match, use window["inner" + Name]
-					size = window["inner" + Name];
-					is_client_width = false;
-				}
-				// Cleanup
-				documentElement.removeChild(bodyElement);
-			}
-		}
-		else {
-			// Default to use window["inner" + Name]
-			size = window["inner" + Name];
-		}
-		return size;
-	};
-
-})(this);
-
-/**
- *
- */
-var RespBreak = (function (window, document, undefined) {
-	/**
-	 * Handles breakpoints in responsive designs, using a pattern inspired
-	 * by Ruby on Rails up()-down() DB Migrator pattern.
-	 *
-	 * @constructor
-	 * @param {Array} states Array of state objects
-	 * @param {number} loaded_version Index of states parameter which is currently loaded (i.e. the default state of the HTML document).
-	 * @version 1.0
+		https://github.com/tysonmatanich/viewportSize/
 	 */
-	var RespBreak = function (states, loaded_version) {
-		var resize_cb = this.getResizeCb();
-		
-		this.states = states;
-		this.loaded_version = loaded_version;
-		
-		if (window.addEventListener) {
-			window.addEventListener("resize", resize_cb, false);
-		} else {
-			window.attachEvent("onresize", resize_cb);
-		}
-		
-		this.resize();
-	};
+	_getWidth: function () {
+		var width,
+				de = document.documentElement,
+				body,
+				div;
 
-	RespBreak.prototype.getStateVersion = function () {
-		var state,
-			width,
-			ver;
-			
-		width = viewportSize.getWidth();
-		
-		for (ver = 0; ver < this.states.length; ver++) {
-			state = this.states[ver];
-			if (state.ishere(width)) {
-				return parseInt(ver, 10);
-			}
-		}
-		
-		return null;
-	};
-
-	RespBreak.prototype.getResizeCb = function () {
-		var obj = this;
-		return function() {
-			obj.resize();
-		};
-	};
-
-	RespBreak.prototype.resize = function () {
-		var from_version = this.loaded_version,
-			to_version = this.getStateVersion(),
-			is_upgrade = to_version >= from_version,
-			phases = [],
-			origin_ind,
-			terminal_ind,
-			ind,
-			phasei,
-			state,
-			version;
-			
-		if (from_version === to_version) {
-			return;
-		}
-			
-		if (is_upgrade) {
-			origin_ind = to_version;
-			terminal_ind = from_version + 1;
-		} else {
-			origin_ind = from_version;
-			terminal_ind = to_version + 1;
-		}
-		
-		ind = origin_ind;
-		while (Math.abs(ind - terminal_ind + 1) > 0.01) {
-			phases.push(ind);
-			ind -= 1;
-		}
-		
-		if (is_upgrade) {
-			phases.reverse();
-		}
-		
-		for (phasei = 0; phasei < phases.length; phasei++) {
-			version = phases[phasei];
-			state = this.states[version];
-			if (is_upgrade) {
-				state.up();
+		if (typeof window.innerWidth === "undefined") {
+			width = de.clientWidth;
+		} else if (window.innerWidth !== de.clientWidth) {
+			if (typeof this.isCW !== "undefined") {
+				// we've cached the width that was accurate
+				width = this.isCW ? de.clientWidth : window.innerWidth;
 			} else {
-				state.down();
+				body = document.createElement("body");
+				body.id = "lVW";
+				body.style.cssText = "overflow:scroll";
+				div = document.createElement("div");
+				div.id = "qVo";
+				div.style.cssText = "position:absolute;top:-1000px";
+				div.innerHTML = "<style>@media(width:" + de.clientWidth + "px){body#lVW div#qVo{width:7px!important}}</style>";
+				body.appendChild(div);
+				de.insertBefore(body, document.head);
+
+				if (div.offsetWidth == 7) {
+					width = de.clientWidth;
+					this.isCW = true;
+				} else {
+					size = window.innerWidth;
+					this.isCW = false;
+				}
+
+				de.removeChild(body);
+			}
+		} else {
+			width = window.innerWidth;
+		}
+
+		return width;
+	},
+	init: function () {
+		var resizeCb,
+				self = this;
+
+		this.states = [];
+		this.cbs = {};
+		this.prevWidth = 0;
+		resizeCb = function () {
+			self.resize();
+		};
+
+		if (window.addEventListener) {
+			window.addEventListener("resize", resizeCb, false);
+		} else {
+			window.attachEvent("onresize", resizeCb);
+		}
+	},
+	addState: function (stateName, minWidth, maxWidth) {
+		if (!maxWidth) {
+			maxWidth = 10e8;
+		}
+
+		this.states.push({
+			name: stateName,
+			min: minWidth,
+			max: maxWidth
+		});
+	},
+	_addCb: function (name, action, cbFun) {
+		var self = this,
+				cb,
+				state,
+				i;
+
+		if (typeof this.cbs[name] === "undefined") {
+			this.cbs[name] = {};
+		}
+
+		if (typeof this.cbs[name][action] === "undefined") {
+			this.cbs[name][action] = [];
+		}
+
+		for (i = 0; i < this.states.length; i++) {
+			if (this.states[i].name === name) {
+				state = this.states[i];
 			}
 		}
-		
-		this.loaded_version = to_version;
-	};
-	
-	return RespBreak;
-})(window, document);
+
+		cb = {
+			fun: cbFun,
+			prevWidth: -10e8
+		};
+		this.cbs[name][action].push(cb);
+		self.resizeCb(cb, state, action, this._getWidth());
+	},
+	enter: function (stateName, cb) {
+		return this._addCb(stateName, "enter", cb);
+	},
+	strictEnter: function (stateName, cb) {
+		return this._addCb(stateName, "strictEnter", cb);
+	},
+	exit: function (stateName, cb) {
+		return this._addCb(stateName, "exit", cb);
+	},
+	strictExit: function (stateName, cb) {
+		return this._addCb(stateName, "strictExit", cb);
+	},
+	resizeCb: function (cb, state, action, curWidth) {
+		var canCall = false,
+				prevWidth = cb.prevWidth;
+
+		switch (action) {
+			case "enter":
+				canCall = (!(prevWidth >= state.min && prevWidth <= state.max) &&
+						(curWidth >= state.min && curWidth <= state.max));
+				break;
+			case "strictEnter":
+				canCall = ((prevWidth < state.min && curWidth >= state.min) ||
+						(prevWidth > state.max && curWidth <= state.max));
+				break;
+			case "exit":
+				canCall = (prevWidth >= state.min && prevWidth <= state.max &&
+						!(curWidth >= state.min && curWidth <= state.max));
+				break;
+			case "strictExit":
+				canCall = ((prevWidth <= state.max && curWidth > state.max) ||
+						(prevWidth >= state.min && curWidth < state.min));
+				break;
+		}
+
+		cb.prevWidth = curWidth;
+		if (canCall) {
+			cb.fun();
+		}
+	},
+	resize: function () {
+		var state,
+				name,
+				action,
+				curWidth = this._getWidth(),
+				i,
+				j,
+				increasing = curWidth > this.prevWidth,
+				actions = ["enter", "exit", "strictEnter", "strictExit"],
+				k;
+
+		for (i = 0; i < this.states.length; i++) {
+			state = this.states[increasing ? i : (this.states.length - i - 1)];
+			name = state.name;
+
+			for (k = 0; k < actions.length; k++) {
+				action = actions[k];
+
+				if (this.cbs[name][action]) {
+					for (j = 0; j < this.cbs[name][action].length; j++) {
+						this.resizeCb(
+							this.cbs[name][action][j],
+							state,
+							action,
+							curWidth
+						);
+					}
+				}
+			}
+		}
+
+		this.prevWidth = curWidth;
+	}
+};
+
+RespBreak.init();
